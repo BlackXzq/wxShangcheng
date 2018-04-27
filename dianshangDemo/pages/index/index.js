52437299ce2636ca8cc5473e06497307a0233ba7
xuzhanqiang <xuzhanqiang@anji-allways.com>
2018-04-27 17:04:26 +0800

    优化首页

--- dianshangDemo/pages/index/index.js
+++ dianshangDemo/pages/index/index.js
@@ -13,11 +13,36 @@ Page({
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
-    indicatorDots: true,
-    autoplay: true,
-    interval: 3000,
-    duration: 1000
+    types: ["全部", "上装", "裤装","家居内衣","特价区","裙装","套装","外套","秒杀"],
+    selectTypeIndex:0,
+    notices:["商铺新开张，优惠多多，戳 戳 我看详情",
+             "大甩卖，大甩卖，老板带着秘书跑路了～～",
+             "耐克店庆100周年，全场5折。。。"],
+    coupons:[
+      {
+        price:40,
+        fullPrice:300,
+        validDay:15
+      },
+      {
+        price: 25,
+        fullPrice: 200,
+        validDay: 10
+      },
+      {
+        price: 10,
+        fullPrice: 100,
+        validDay: 7      
+      }
+    ]
   },
+
+  typeItemClick: function(e) {
+    var index = e.currentTarget.dataset.index;
+    this.setData({
+      selectTypeIndex:index
+    });
+  }, 
 
   /**
    * 生命周期函数--监听页面加载
